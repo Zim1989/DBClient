d27dfc21ff093e04d7868e6433a9bf021759b637
Marco <Marco@Marco-PC.fritz.box>
2015-02-11 16:27:38 +0100

    neu

--- src/Frames/MainFrame.java
+++ src/Frames/MainFrame.java
@@ -36,21 +36,10 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         mainPanel1 = new Panels.MainPanel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Bauprojekt-Verwaltung");
         getContentPane().add(mainPanel1, java.awt.BorderLayout.CENTER);
-
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
-
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
-
-        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -61,9 +50,6 @@ public class MainFrame extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
     private Panels.MainPanel mainPanel1;
     // End of variables declaration//GEN-END:variables
 }
